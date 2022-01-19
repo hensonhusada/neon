02ed6f5f0bcaf35d2fa056569b68dce30875430e
Henson Husada <hensonhusada@gmail.com>
2022-01-19 10:58:20 +0700

    Fixed file naming & configurations

--- apps/schemas/SchemaCIF.py
+++ apps/schemas/SchemaCIF.py
@@ -1,3 +1,4 @@
+import datetime
 from pydantic import BaseModel
 from typing import Optional, List
 
@@ -8,10 +9,27 @@ class RequestCIF(BaseModel):
 
 class CIF(BaseModel):
     loanid: str = None
+    loan_type : str = None
+    loan_status : str = None
     cif: str  = None
-    tenor: int  = None
-    amount: int  = None
-    limit: int  = None
+    loan_tenure : int = None
+    loan_amount: int  = None
+    interest : int = None
+    idno : str = None
+    fname : str = None
+    lname : str = None
+    dob : datetime.date = None
+    gender : str = None
+    marital_status : str = None
+    income : int = None
+    phone : str = None
+    email : str = None
+    isphoneverified : str = None
+    isemailverified : str = None
+    createdate : datetime.date = None
+    updatedate : datetime.date = None
+    source : str = None
+    # limit: int  = None
 
 
 class ResponseCIF(BaseModel):
